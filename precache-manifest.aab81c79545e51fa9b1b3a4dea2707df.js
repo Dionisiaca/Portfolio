9bec99ebac2c3e6483a87c44c69edfc66f1f428b
26erika <develop.erika@gmail.com>
2020-05-01 13:38:52 -0500

    Updates

---
+++ precache-manifest.aab81c79545e51fa9b1b3a4dea2707df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c49c8f9e80173656f14db4cad8c1fa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "30dfee7fef744ff2b530",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "43e235e8226390d55214",
+    "url": "/portfolio/static/js/2.368c59a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.368c59a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30dfee7fef744ff2b530",
+    "url": "/portfolio/static/js/main.061432a1.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
